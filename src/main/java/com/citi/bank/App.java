0aebdd147f1c09da42d2fb3b5dacaf9e0cea32c7
N-Anitha <anithakrishna060@gmail.com>
2026-03-19 17:15:44 +0530

    Create App.java

---
+++ src/main/java/com/citi/bank/App.java
@@ -0,0 +1,10 @@
+package com.citi.bank;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Anitha! Citibank App is Running on AKS...");
+        while(true) {
+            try { Thread.sleep(10000); } catch (Exception e) {}
+        }
+    }
+}
